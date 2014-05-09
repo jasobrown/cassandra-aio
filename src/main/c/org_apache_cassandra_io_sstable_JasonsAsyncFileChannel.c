091ee8155b3bdcf747ca314aa85d12dc40d99a1f
Jason Brown <jasobrown@apple.com>
2014-05-09 02:34:03 -0700

    WIP

--- src/main/c/org_apache_cassandra_io_sstable_JasonsAsyncFileChannel.c
+++ src/main/c/org_apache_cassandra_io_sstable_JasonsAsyncFileChannel.c
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include "org_apache_cassandra_io_sstable_JasonsAsyncFileChannel.h"
 
-jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+jint JNI_OnLoad(JavaVM* vm, void* reserved)
+{
     JNIEnv* env;
-    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK)
+    {
         return JNI_ERR;
     }
     return 0;
+}
+
+JNIEXPORT jint JNICALL Java_org_apache_cassandra_io_sstable_JasonsAsyncFileChannel_getNativeVersion
+        (JNIEnv * env, jclass ref)
+{
+    return org_apache_cassandra_io_sstable_JasonsAsyncFileChannel_EXPECTED_NATIVE_VERSION;
 }
